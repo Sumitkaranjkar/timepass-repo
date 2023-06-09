4b2f778ecac8733c265869abf8997b849d283326
Sumitkaranjkar <98700773+Sumitkaranjkar@users.noreply.github.com>
2023-06-09 21:02:59 +0530

    osadd

---
+++ osp/8.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+filename="example.txt"
+echo "File opened."
+
+# Open the file for writing
+exec 3>$filename
+
+# Write to the file
+echo "Hello, World!" >&3
+
+# Close the file
+exec 3>&-
+
+echo "File closed."
+
